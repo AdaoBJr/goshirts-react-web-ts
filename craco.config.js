1eb48e828a3dbb4924cc2db698abd4c8bb82b029
AdaoBJr <adaobjr@gmail.com>
2022-04-13 21:18:51 -0400

    feat: :tada: starting web-ts project

---
+++ craco.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { getLoader, loaderByName } = require('@craco/craco');
+
+const packages = [];
+packages.push(path.join(__dirname, '../lib'));
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, arg) => {
+      const { isFound, match } = getLoader(webpackConfig, loaderByName('babel-loader'));
+      if (isFound) {
+        const include = Array.isArray(match.loader.include)
+          ? match.loader.include
+          : [match.loader.include];
+
+        match.loader.include = include.concat(packages);
+      }
+
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+      );
+
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+
+      return webpackConfig;
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">APP COM TS</div>;
+}
+
+export default App;
