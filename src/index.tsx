e73262a6c81d48c02e22f7616ea35fd3122108bb
AdaoBJr <adaobjr@gmail.com>
2022-04-15 16:13:46 -0400

    feat: :sparkles: config ApolloProvider on Index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,41 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.module.css';
 
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HASURA_URL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>
 );
