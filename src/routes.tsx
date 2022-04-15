71924f9c432a36bffe56c6d5267bc9eb5c96d189
AdaoBJr <adaobjr@gmail.com>
2022-04-15 16:41:07 -0400

    feat: :art: updated format Router Component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,16 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import CreateAccount from './lib/pages/CreateAccount';
 import SignIn from './lib/pages/SignIn';
 
-function Router() {
+const Router: React.FC = () => {
   return (
     <Routes>
       <Route path="/create-account" element={<CreateAccount />} />
       <Route path="/login" element={<SignIn />} />
     </Routes>
   );
-}
+};
 
 export default Router;
